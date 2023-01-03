5d094334afdda292cf8b6f8d367dca27d4dcb5bd
Kulbicki Andrey <kubl-ml@mail.ru>
2023-01-03 12:21:47 +0300

    Spring Boot

---
+++ src/main/java/com/andrew/sprigbootapp/SprigBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.andrew.sprigbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprigBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprigBootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrew/sprigbootapp/SprigBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrew.sprigbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprigBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
